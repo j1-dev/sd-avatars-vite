72a73e727d4d0a7da419c736e12deaea668cb43d
davidsed9 <davidsed9@gmail.com>
2023-04-04 11:47:16 +0100

    button/sizes halfway fixed

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,6 +1,5 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import anime from 'animejs/lib/anime.es.js';
-import $ from 'jquery';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,32 +9,44 @@ library.add(faCheck);
 
 function SubmitButton({ onSubmit }) {
   const [showCheck, setShowCheck] = useState(false);
+  const buttonRef = useRef();
 
   function handleClick() {
-    $('#button').addClass('onclic');
+    anime({
+      targets: buttonRef.current,
+      scale: [
+        { value: 1, duration: 0 },
+        { value: 1.2, duration: 200 },
+        { value: 1, duration: 200 }
+      ],
+      borderRadius: [
+        { value: '50%', duration: 200, delay: 200 },
+        { value: '0%', duration: 200 }
+      ],
+      complete: function () {
+        setShowCheck(true);
+        if (onSubmit) {
+          onSubmit();
+        }
 
-    setTimeout(function () {
-      $('#button').removeClass('onclic');
-      $('#button').addClass('validate');
-      setShowCheck(true);
-      if (onSubmit) {
-        onSubmit();
+        setTimeout(() => {
+          setShowCheck(false);
+        }, 2250);
       }
-    }, 2250);
-
-    setTimeout(function () {
-      $('#button').removeClass('validate');
-      setShowCheck(false);
-    }, 4500);
+    });
   }
 
   return (
     <div className="absolute top-1/2 left-1/2 -mt-10 -ml-16 w-32 h-10 text-center">
       <button
-        id="button"
+        ref={buttonRef}
         onClick={handleClick}
-        className="outline-none h-10 w-32 rounded-full bg-white border-2 border-green-500 text-green-500 tracking-wider text-shadow-none text-xs font-bold cursor-pointer transition-all duration-200 ease-in">
-        {showCheck && <FontAwesomeIcon icon="check" className="check-icon" />}
+        className="outline-none h-10 w-32 rounded-full bg-green-500 border-2 border-green-500 text-white tracking-wider text-shadow-none text-xs font-bold cursor-pointer transition-all duration-200 ease-in">
+        {showCheck ? (
+           <FontAwesomeIcon icon="check" className="check-icon" />
+        ) : ( 
+          "Generar"
+        )}
       </button>
     </div>
   );
